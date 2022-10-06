cb49054a6194ae70ccd274fe143f7f4123648349
pdao1 <69880265+pdao1@users.noreply.github.com>
2022-10-06 05:07:32 -1000

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import { Button, Stack } from "react-bootstrap";
+import BudgetCard from './components/BudgetCard'
 
 function App() {
-    return "Abc"
+	return (
+		<Container>
+			<Stack direction='horizontal' gap='2' className='mb-4'>
+				<h1 className='me-auto'>Budgets</h1>
+				<Button variant='primary'>Add Budget</Button>
+				<Button variant='outline-primary'>Add Expense</Button>
+			</Stack>
+			<div
+				style={{
+					display: "grid",
+					gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr)",
+					gap: "1rem",
+					alignItems: "flex-start",
+				}}
+			>
+
+                <BudgetCard name="Entertainment" amount={800} max={1000}></BudgetCard>
+            </div>
+		</Container>
+	);
 }
 
-export default App
+export default App;
